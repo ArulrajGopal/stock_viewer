b8967607b52041c8135a1430879f370e37116ef3
Arulraj Gopal <arulrajgopal@outlook.com>
2024-04-28 10:27:20 +0000

    update

--- stock_viewer/src/main/java/pack1/test1.java
+++ stock_viewer/src/main/java/pack1/test1.java
@@ -7,17 +7,14 @@ public class test1 {
         utils obj = new utils();
         String message = obj.fetchStockDetails("PHARMA");
 
-        JSONArray result_jsonarr = new JSONArray(message);
+        // JSONArray result_jsonarr = new JSONArray(message);
+        // JSONObject element = (JSONObject) result_jsonarr.get(0);
+        // String symbol = element.getString("symbol");
 
-        JSONObject element = (JSONObject) result_jsonarr.get(0);
-
-        String symbol = element.getString("symbol");
-        System.out.println(symbol);
-
-        System.out.println(message);
-        System.out.println(result_jsonarr);
-        System.out.println(element);
-        System.out.println(symbol);
+        // System.out.println(message);
+        // System.out.println(result_jsonarr);
+        // System.out.println(element);
+        // System.out.println(symbol);
 
     }
     
