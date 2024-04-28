84be15f947e72823b081454ac9ed5446c6135603
Arulraj Gopal <arulrajgopal@outlook.com>
2024-04-28 10:25:41 +0000

    update

--- stock_viewer/src/main/java/pack1/test1.java
+++ stock_viewer/src/main/java/pack1/test1.java
@@ -1,12 +1,25 @@
 package pack1;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class test1 {
     public static void main(String[] args) {
         utils obj = new utils();
         String message = obj.fetchStockDetails("PHARMA");
-        Object json_obj = obj.StrToJsonConvert(message);
+
+        JSONArray result_jsonarr = new JSONArray(message);
+
+        JSONObject element = (JSONObject) result_jsonarr.get(0);
+
+        String symbol = element.getString("symbol");
+        System.out.println(symbol);
+
         System.out.println(message);
-        System.out.println(json_obj);
+        System.out.println(result_jsonarr);
+        System.out.println(element);
+        System.out.println(symbol);
+
     }
     
 }
+
