75195764b709caca4c9b658d1016dacb3d60fcde
Arulraj Gopal <arulrajgopal@outlook.com>
2024-04-28 08:15:35 +0000

    update

---
+++ stock_viewer/src/main/java/pack1/test1.java
@@ -0,0 +1,9 @@
+package pack1;
+
+public class test1 {
+    public static void main(String[] args) {
+        long currentTimeMillis = System.currentTimeMillis();
+        System.out.println("Current timestamp in milliseconds: " + currentTimeMillis);
+    }
+}
+
