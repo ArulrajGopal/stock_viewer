37187ac9609adc35a2d53ac0744dc8a5b63d9a1e
arulraj <arulrajgopal@outlook.com>
2024-05-01 12:04:49 +0000

    update

--- stock_viewer/src/main/java/pack1/main_file.java
+++ stock_viewer/src/main/java/pack1/main_file.java
@@ -15,6 +15,10 @@ class MyThread extends Thread {
         else if (parameter == "consumer_finservice"){ obj.KafConsumer("finservice");}
         else if (parameter == "producer_pharma"){ obj.KafProducer("pharma");}
         else if (parameter == "consumer_pharma"){ obj.KafConsumer("pharma");}
+        else if (parameter == "producer_energy"){ obj.KafProducer("energy");}
+        else if (parameter == "consumer_energy"){ obj.KafConsumer("energy");}
+        else if (parameter == "producer_fmcg"){ obj.KafProducer("fmcg");}
+        else if (parameter == "consumer_fmcg"){ obj.KafConsumer("fmcg");}
 
     }
 
@@ -26,11 +30,19 @@ public class main_file {
         MyThread thread2 = new MyThread("consumer_finservice");
         MyThread thread3 = new MyThread("producer_pharma");
         MyThread thread4 = new MyThread("consumer_pharma");
+        MyThread thread5 = new MyThread("producer_energy");
+        MyThread thread6 = new MyThread("consumer_energy");
+        MyThread thread7 = new MyThread("producer_fmcg");
+        MyThread thread8 = new MyThread("consumer_fmcg");
 
 
         thread1.start();
         thread2.start();
         thread3.start();
         thread4.start();
+        thread5.start();
+        thread6.start();
+        thread7.start();
+        thread8.start();
     }
 }
