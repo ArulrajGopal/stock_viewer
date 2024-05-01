25d5a1ff98c600814d994fa4c16875cbd8c5578e
arulraj <arulrajgopal@outlook.com>
2024-05-01 09:28:31 +0000

    update

--- stock_viewer/src/main/java/pack1/test1.java
+++ stock_viewer/src/main/java/pack1/test1.java
@@ -1,5 +1,7 @@
 package pack1;
 
+import org.json.JSONObject;
+
 // variables = ["PHARMA","ENERGY","FMCG","FIN%20SERVICE"]
 
 public class test1 {
@@ -7,7 +9,14 @@ public class test1 {
     public static void main(String[] args) {
         utils obj = new utils();
         String message = obj.fetchStockDetails("FIN%20SERVICE");
+   
+        JSONObject element = (JSONObject) obj.StrToJsonConvert(message);
+
+        String symbol = element.getString("symbol");
+
         System.out.println(message);
+        System.out.println(element);
+        System.out.println(symbol);
 
     }
 
