44f78f6d8ee26f56ee8e7bd47482dc1f97766abb
arulraj <arulrajgopal@outlook.com>
2024-05-01 09:58:00 +0000

    update

--- stock_viewer/src/main/java/pack1/test1.java
+++ stock_viewer/src/main/java/pack1/test1.java
@@ -2,25 +2,38 @@ package pack1;
 
 
 class MyThread extends Thread {
+    private int parameter;
+
+    public MyThread(int parameter) {
+        this.parameter = parameter;
+    }
+
     public void run() {
-        for (int i = 0; i < 5; i++) {
-            System.out.println("thread : " + i);
-            try {
-                Thread.sleep(1000); // Sleep for 1 second
-            } catch (InterruptedException e) {
-                System.out.println("Thread interrupted");
-            }
+        if (parameter % 2 == 0) {
+            evenMethod();
+        } else {
+            oddMethod();
         }
+    }
+
+    private void evenMethod() {
+        System.out.println("Even thread with parameter " + parameter);
+    }
+
+    private void oddMethod() {
+        System.out.println("Odd thread with parameter " + parameter);
     }
 }
 
 public class test1 {
     public static void main(String[] args) {
-        MyThread thread1 = new MyThread();
-        MyThread thread2 = new MyThread();
+        MyThread thread1 = new MyThread(2);
+        MyThread thread2 = new MyThread(3);
 
-        thread1.start(); // Starts the first thread
-        thread2.start(); // Starts the second thread
+        thread1.start();
+        thread2.start();
 
+        // Main thread continues independently
+        System.out.println("Main thread continues...");
     }
 }
