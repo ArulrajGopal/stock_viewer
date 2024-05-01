4114ae7353d5b6d2eeb75e6aae6b63ae401da670
arulraj <arulrajgopal@outlook.com>
2024-05-01 11:04:36 +0000

    update

--- stock_viewer/src/main/java/pack1/master.java
+++ stock_viewer/src/main/java/pack1/master.java
@@ -3,34 +3,35 @@ package pack1;
 
 class MyThread extends Thread {
     private String parameter;
+    utils obj = new utils();
 
     public MyThread(String parameter) {
         this.parameter = parameter;
     }
 
     public void run() {
-        if (parameter == "producer") { producermethod(); }
-        else if (parameter == "consumer"){ consumermethod(); }
+        System.out.println(parameter);
+        // if (parameter == "producer") { producermethod(); }
+        // else if (parameter == "consumer"){ consumermethod(); }
 
     }
 
-    private void consumermethod() {
-        utils obj = new utils();
-        obj.KafConsumer("finservice");
-    }
+    // private void consumermethod() {obj.KafConsumer("finservice");}
+    // private void producermethod() {obj.KafProducer("finservice");}
 
-    private void producermethod() {
-        utils obj = new utils();
-        obj.KafProducer("finservice");
-    }
 }
 
 public class master {
     public static void main(String[] args) {
-        MyThread thread1 = new MyThread("producer");
-        MyThread thread2 = new MyThread("consumer");
+        MyThread thread1 = new MyThread("producer#finservice");
+        MyThread thread2 = new MyThread("consumer#finservice");
+        MyThread thread3 = new MyThread("producer#pharma");
+        MyThread thread4 = new MyThread("consumer#pharma");
+
 
         thread1.start();
         thread2.start();
+        thread3.start();
+        thread4.start();
     }
 }
