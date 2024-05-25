165004879cccebee3f436ebd0edeac1c2fdb5d30
arulraj <arulrajgopal@outlook.com>
2024-05-25 08:44:47 +0000

    update

---
+++ stock_viewer/src/main/java/pack1/test1.java
@@ -0,0 +1,11 @@
+package pack1;
+
+public class test1 {
+
+    public static void main(String[] args) {
+    utils obj = new utils();
+    String result = obj.fetchStockDetails ("pharma");
+    System.out.println(result);
+    }
+    
+}
