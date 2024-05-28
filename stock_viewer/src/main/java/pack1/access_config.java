9e1999d53475ab29ed981a90b94ac596861b8977
Arulraj <arulrajgopal@outlook.com>
2024-05-28 11:47:05 +0000

    update

---
+++ stock_viewer/src/main/java/pack1/access_config.java
@@ -0,0 +1,33 @@
+package pack1;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+
+
+public class access_config {
+
+    public JSONArray get_identifier_list (String sector) throws IOException {
+
+        String filePath = "stock_viewer/src/main/java/pack1/config.json"; 
+        String content = new String(Files.readAllBytes(Paths.get(filePath)));
+        JSONObject jsonObject = new JSONObject(content);
+
+        JSONArray result = jsonObject.getJSONArray(sector);
+
+        return result;
+
+
+    }
+
+}
+
+
+
+
+
---
+++ stock_viewer/src/main/java/pack1/test1.java
@@ -0,0 +1,12 @@
+package pack1;
+
+import java.io.IOException;
+
+public class test1 {
+
+    public static void main(String[] args) throws IOException {
+        access_config obj = new access_config();
+        System.out.println(obj.get_identifier_list("BANK"));
+    }
+    
+}
