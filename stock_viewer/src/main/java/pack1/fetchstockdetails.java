6c64f81ea795c814562fe58498acfaaa62608ba4
Arulraj <arulrajgopal@outlook.com>
2024-05-28 10:11:39 +0000

    udpate

--- stock_viewer/src/main/java/pack1/fetchstockdetails.java
+++ stock_viewer/src/main/java/pack1/fetchstockdetails.java
@@ -8,22 +8,20 @@ import java.net.http.HttpResponse;
 
 public class fetchstockdetails {
 
-    public static String main(String[] args) {
+    public static void main(String[] args) {
 
         fetchstockdetails obj = new fetchstockdetails();
 
         String result = obj.fetchStockDetails();
 
         System.out.println(result);
-        return result;
-
 
     }
 
     public String fetchStockDetails () {
                 try {
                     HttpRequest request = HttpRequest.newBuilder()
-                        .uri(URI.create("https://latest-stock-price.p.rapidapi.com"))
+                        .uri(URI.create("https://latest-stock-price.p.rapidapi.com/any"))
                         .header("X-RapidAPI-Key", "23a945bde0msha8c00c2faa561f5p119d77jsnea9895e0d88d")
                         .header("X-RapidAPI-Host", "latest-stock-price.p.rapidapi.com")
                         .method("GET", HttpRequest.BodyPublishers.noBody())
