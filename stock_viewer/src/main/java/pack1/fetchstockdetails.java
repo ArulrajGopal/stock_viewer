839e163ab691ebfbb635b245271d0f744343e79d
Arulraj <arulrajgopal@outlook.com>
2024-05-28 10:35:05 +0000

    update

--- stock_viewer/src/main/java/pack1/fetchstockdetails.java
+++ stock_viewer/src/main/java/pack1/fetchstockdetails.java
@@ -5,15 +5,15 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class fetchstockdetails {
 
     public static void main(String[] args) {
-
         fetchstockdetails obj = new fetchstockdetails();
-
-        String result = obj.fetchStockDetails();
-
+        String result = obj.fetchStockDetails_through_aws_api_gateway();
         System.out.println(result);
 
     }
@@ -28,11 +28,36 @@ public class fetchstockdetails {
                         .build();
 
                     HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
-                     return response.body(); }
+                     return response.body(); 
+                }
 
-                catch (IOException | InterruptedException e) {e.printStackTrace(); }
-
-                return null; 
+                catch (IOException | InterruptedException e) {
+                    e.printStackTrace(); 
+                    return null; 
+                }                
         }
+
+
+
+
+    public String fetchStockDetails_through_aws_api_gateway () {
+                try {
+                    OkHttpClient client = new OkHttpClient();
+
+                    String url = "https://qmxqg0gnxh.execute-api.us-east-1.amazonaws.com/prod/myresource";
+                    Request request = new Request.Builder()
+                                            .url(url)
+                                            .build();
+                    Response response = client.newCall(request).execute();
+                    return response.body().string();
+                }
+
+                catch (IOException e) {
+                    e.printStackTrace(); 
+                    return null; 
+                    
+                }
+
+    }
     
 }
