25a28b9653965f31181975a68327ec4e68772edf
Arulraj <arulrajgopal@outlook.com>
2024-05-29 16:53:48 +0000

    update

--- stock_viewer/src/main/java/ddl/create_kafka_topics.java
+++ stock_viewer/src/main/java/ddl/create_kafka_topics.java
@@ -1,12 +1,45 @@
 package ddl;
 
-import pack1.kaf_prod_cons;
+import org.apache.kafka.clients.admin.AdminClient;
+import org.apache.kafka.clients.admin.AdminClientConfig;
+import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.common.errors.TopicExistsException;
+
+import java.util.Collections;
+import java.util.Properties;
+import java.util.concurrent.ExecutionException;
 
 public class create_kafka_topics {
     public static void main(String[] args) {
-        kaf_prod_cons obj = new kaf_prod_cons();
 
-        obj.KafProducer("HEALTHCARE");
+         String bootstrapServers = "localhost:9092";
+         int numPartitions = 3;
+         short replicationFactor = 1;
+
+        // Create the AdminClient configuration
+        Properties config = new Properties();
+        config.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+
+
+         String topicName = "bank_tp";
+ 
+         // Create the AdminClient
+         try (AdminClient adminClient = AdminClient.create(config)) {
+             // Define the new topic
+             NewTopic newTopic = new NewTopic(topicName, numPartitions, replicationFactor);
+ 
+             // Create the topic
+             adminClient.createTopics(Collections.singletonList(newTopic)).all().get();
+ 
+             System.out.println("Topic created successfully");
+         } 
+
+
+         catch (InterruptedException | ExecutionException | TopicExistsException e) {
+            System.out.println("Failed to create topic or topic already exists ");
+
+        }
+     }
+
     }
     
-}
--- stock_viewer/src/main/java/pack1/test1.java
+++ stock_viewer/src/main/java/pack1/test1.java
@@ -1,14 +1,10 @@
 package pack1;
 
-import java.io.IOException;
-
 public class test1 {
-        public static void main(String[] args) throws IOException  {
+    public static void main(String[] args) {
+        kaf_prod_cons obj = new kaf_prod_cons();
 
-            access_config obj = new access_config();
-            obj.get_topic_name("HEALTHCARE");
-
-
+        obj.KafProducer("HEALTHCARE");
     }
     
 }
