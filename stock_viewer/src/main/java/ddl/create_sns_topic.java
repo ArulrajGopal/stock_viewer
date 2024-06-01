5a83de48e16d88baeee42088da212197dd5cc322
Arulraj <arulrajgopal@outlook.com>
2024-06-01 13:15:49 +0000

    update

--- stock_viewer/src/main/java/ddl/create_sns_topic.java
+++ stock_viewer/src/main/java/ddl/create_sns_topic.java
@@ -9,33 +9,24 @@ import software.amazon.awssdk.services.sns.model.SnsException;
 public class create_sns_topic {
     public static void main(String[] args) {
 
-
         String topicName = "MyTopic2";
-        System.out.println("Creating a topic with name: " + topicName);
-        SnsClient snsClient = SnsClient.builder()
-                .region(Region.US_EAST_1)
-                .build();
+        SnsClient snsClient = SnsClient.builder().region(Region.US_EAST_1).build();
 
-        String arnVal = createSNSTopic(snsClient, topicName);
-        System.out.println("The topic ARN is" + arnVal);
-        snsClient.close();
-    }
-
-    public static String createSNSTopic(SnsClient snsClient, String topicName) {
-        CreateTopicResponse result;
         try {
             CreateTopicRequest request = CreateTopicRequest.builder()
                     .name(topicName)
                     .build();
 
-            result = snsClient.createTopic(request);
-            return result.topicArn();
+            CreateTopicResponse result = snsClient.createTopic(request);
+            System.out.println(result.topicArn());
+
 
         } catch (SnsException e) {
             System.err.println(e.awsErrorDetails().errorMessage());
             System.exit(1);
         }
-        return "";
+
+        
     }
 }
 
