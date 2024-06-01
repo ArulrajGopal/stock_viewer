a79a6c186a9ccf99ca95e2fbeace34465a98aedf
Arulraj <arulrajgopal@outlook.com>
2024-06-01 13:12:55 +0000

    update

--- stock_viewer/src/main/java/ddl/create_sns_topic.java
+++ stock_viewer/src/main/java/ddl/create_sns_topic.java
@@ -1,15 +1,16 @@
 package ddl;
-
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.sns.SnsClient;
 import software.amazon.awssdk.services.sns.model.CreateTopicRequest;
 import software.amazon.awssdk.services.sns.model.CreateTopicResponse;
 import software.amazon.awssdk.services.sns.model.SnsException;
 
+
 public class create_sns_topic {
     public static void main(String[] args) {
 
-        String topicName = "MyTopic1";
+
+        String topicName = "MyTopic2";
         System.out.println("Creating a topic with name: " + topicName);
         SnsClient snsClient = SnsClient.builder()
                 .region(Region.US_EAST_1)
