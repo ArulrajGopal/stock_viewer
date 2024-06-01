57c9d1870a3b967ff512128e568b692652496fb5
Arulraj <arulrajgopal@outlook.com>
2024-06-01 11:41:20 +0000

    update

--- stock_viewer/src/main/java/ddl/create_sns_topic.java
+++ stock_viewer/src/main/java/ddl/create_sns_topic.java
@@ -1,22 +1,40 @@
 package ddl;
 
+import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.sns.SnsClient;
 import software.amazon.awssdk.services.sns.model.CreateTopicRequest;
 import software.amazon.awssdk.services.sns.model.CreateTopicResponse;
+import software.amazon.awssdk.services.sns.model.SnsException;
 
 public class create_sns_topic {
     public static void main(String[] args) {
-        SnsClient snsClient = SnsClient.builder().build();
-        CreateTopicRequest request = CreateTopicRequest.builder()
-                .name("MyTopic1")
+
+        String topicName = "MyTopic1";
+        System.out.println("Creating a topic with name: " + topicName);
+        SnsClient snsClient = SnsClient.builder()
+                .region(Region.US_EAST_1)
                 .build();
 
-        CreateTopicResponse result = snsClient.createTopic(request);
-
-        // Print out the topic ARN
-        String topicArn = result.topicArn();
-        System.out.println("Created topic ARN: " + topicArn);
-
+        String arnVal = createSNSTopic(snsClient, topicName);
+        System.out.println("The topic ARN is" + arnVal);
         snsClient.close();
     }
+
+    public static String createSNSTopic(SnsClient snsClient, String topicName) {
+        CreateTopicResponse result;
+        try {
+            CreateTopicRequest request = CreateTopicRequest.builder()
+                    .name(topicName)
+                    .build();
+
+            result = snsClient.createTopic(request);
+            return result.topicArn();
+
+        } catch (SnsException e) {
+            System.err.println(e.awsErrorDetails().errorMessage());
+            System.exit(1);
+        }
+        return "";
+    }
 }
+
