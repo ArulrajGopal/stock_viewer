58d990133e83f9ff2f2707a57f83d6c6ece543f0
Arulraj <arulrajgopal@outlook.com>
2024-06-01 10:39:11 +0000

    update

--- stock_viewer/src/main/java/pack1/main_file.java
+++ stock_viewer/src/main/java/pack1/main_file.java
@@ -1,48 +1,48 @@
-// package pack1;
+package pack1;
 
 
-// class MyThread extends Thread {
-//     private String parameter;
-//     utils obj = new utils();
+class MyThread extends Thread {
+    private String parameter;
+    kaf_prod_cons obj = new kaf_prod_cons();
 
-//     public MyThread(String parameter) {
-//         this.parameter = parameter;
-//     }
+    public MyThread(String parameter) {
+        this.parameter = parameter;
+    }
 
-//     public void run() {
+    public void run() {
 
-//         if (parameter == "producer_finservice") { obj.KafProducer("finservice"); }
-//         else if (parameter == "consumer_finservice"){ obj.KafConsumer("finservice");}
-//         else if (parameter == "producer_pharma"){ obj.KafProducer("pharma");}
-//         else if (parameter == "consumer_pharma"){ obj.KafConsumer("pharma");}
-//         else if (parameter == "producer_energy"){ obj.KafProducer("energy");}
-//         else if (parameter == "consumer_energy"){ obj.KafConsumer("energy");}
-//         else if (parameter == "producer_fmcg"){ obj.KafProducer("fmcg");}
-//         else if (parameter == "consumer_fmcg"){ obj.KafConsumer("fmcg");}
+        if (parameter == "producer_bank") { obj.KafProducer("finservice"); }
+        else if (parameter == "consumer_bank"){ obj.KafConsumer("finservice");}
+        else if (parameter == "producer_energy"){ obj.KafProducer("pharma");}
+        else if (parameter == "consumer_pharma"){ obj.KafConsumer("pharma");}
+        else if (parameter == "producer_energy"){ obj.KafProducer("energy");}
+        else if (parameter == "consumer_energy"){ obj.KafConsumer("energy");}
+        else if (parameter == "producer_fmcg"){ obj.KafProducer("fmcg");}
+        else if (parameter == "consumer_fmcg"){ obj.KafConsumer("fmcg");}
 
-//     }
+    }
 
-// }
+}
 
-// public class main_file {
-//     public static void main(String[] args) {
-//         MyThread thread1 = new MyThread("producer_finservice");
-//         MyThread thread2 = new MyThread("consumer_finservice");
-//         MyThread thread3 = new MyThread("producer_pharma");
-//         MyThread thread4 = new MyThread("consumer_pharma");
-//         MyThread thread5 = new MyThread("producer_energy");
-//         MyThread thread6 = new MyThread("consumer_energy");
-//         MyThread thread7 = new MyThread("producer_fmcg");
-//         MyThread thread8 = new MyThread("consumer_fmcg");
+public class main_file {
+    public static void main(String[] args) {
+        MyThread thread1 = new MyThread("producer_bank");
+        MyThread thread2 = new MyThread("consumer_bank");
+        MyThread thread3 = new MyThread("producer_energy");
+        MyThread thread4 = new MyThread("consumer_energy");
+        MyThread thread5 = new MyThread("producer_fmcg");
+        MyThread thread6 = new MyThread("consumer_fmcg");
+        MyThread thread7 = new MyThread("producer_healthcare");
+        MyThread thread8 = new MyThread("consumer_healthcare");
 
 
-//         thread1.start();
-//         thread2.start();
-//         thread3.start();
-//         thread4.start();
-//         thread5.start();
-//         thread6.start();
-//         thread7.start();
-//         thread8.start();
-//     }
-// }
+        thread1.start();
+        thread2.start();
+        thread3.start();
+        thread4.start();
+        thread5.start();
+        thread6.start();
+        thread7.start();
+        thread8.start();
+    }
+}
