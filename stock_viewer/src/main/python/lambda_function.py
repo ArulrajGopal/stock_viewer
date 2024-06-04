edb36466b9e8368b4473b907e7e1e39c064d8efa
Arulraj <arulrajgopal@outlook.com>
2024-06-04 12:35:07 +0000

    update

--- stock_viewer/src/main/python/lambda_function.py
+++ stock_viewer/src/main/python/lambda_function.py
@@ -9,7 +9,7 @@ def lambda_handler(event, context):
 
             #extracting message from the event
             new_image = event["Records"][0]["dynamodb"]["NewImage"]
-            message = "As of "+str(new_image["last_updated_time"]["S"])+" , the "+ str(new_image["symbol"]["s"]).lower() +" stock was trading at "+ str(new_image["last_traded_price"]["N"]) + " INR"
+            message = "As of "+str(new_image["last_updated_time"]["S"])+" , the "+ str(new_image["symbol"]["S"]).lower() +" stock was trading at "+ str(new_image["last_traded_price"]["N"]) + " INR"
 
 
             # developing topic_arn with event
