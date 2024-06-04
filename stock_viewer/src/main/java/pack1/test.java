5deeb20207b8f3132aae7df1fd7f477be4ca79d0
Arulraj <arulrajgopal@outlook.com>
2024-06-04 10:43:06 +0000

    UPDATE

--- stock_viewer/src/main/java/pack1/test.java
+++ stock_viewer/src/main/java/pack1/test.java
@@ -4,11 +4,11 @@ import java.io.IOException;
 
 public class test {
     public static void main(String[] args) throws IOException {
-        System.out.println("Connection working!");
+        // System.out.println("Connection working!");
 
 
         fetchstockdetails obj = new fetchstockdetails();
-        System.out.println(obj.fetchstockDetails_for_sector("ENERGY"));
+        System.out.println(obj.fetchStockDetails_through_aws_api_gateway());
     }
 }
 
