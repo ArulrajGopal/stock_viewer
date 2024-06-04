455a1f27c283f2a75394539921b57931a7a000df
Arulraj <arulrajgopal@outlook.com>
2024-06-04 06:04:33 +0000

    update

--- stock_viewer/src/main/java/pack1/test.java
+++ stock_viewer/src/main/java/pack1/test.java
@@ -1,8 +1,14 @@
 package pack1;
 
+import java.io.IOException;
+
 public class test {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.out.println("Connection working!");
+
+
+        fetchstockdetails obj = new fetchstockdetails();
+        System.out.println(obj.fetchstockDetails_for_sector("ENERGY"));
     }
 }
 
